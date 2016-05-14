471b712226851db92b2adbcaf0cd5722b6c591e6
FannCyii <954713025@qq.com>
2016-05-14 12:52:35 +0800

    aaa

---
+++ build/CustomCycleScrollView.build/Release-iphoneos/CustomCycleScrollView.build/Objects-normal/armv7/ViewController.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/user/Desktop/Demo/CCCycleScrollView/CustomCycleScrollView/ViewController.m \
+  /Users/user/Desktop/Demo/CCCycleScrollView/CustomCycleScrollView/ViewController.h \
+  /Users/user/Desktop/Demo/CCCycleScrollView/CustomCycleScrollView/CCCycleScrollView/CCCycleScrollView.h
--- testArchive.sh
+++ testArchive.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 echo "准备开始打ipa包...................."
-#工程环境路径workspace_path=.#项目名称project_name=CustomCycleScrollView
-#build的路径build_path=$workspace_path
+#工程环境路径
+workspace_path=.
+#项目名称
+project_name=CustomCycleScrollView
+#build的路径
+build_path=$workspace_path
 echo "第一步，进入项目工程文件: $build_path"
 cd $build_path
 echo "第二步，执行build clean命令"
@@ -10,6 +14,13 @@ echo "第三步，执行编译生成.app命令"
 xcodebuild
 echo "在项目工程文件内生成一个build子目录，里面有${project_name}.App程序"
 echo "第四步, 导出ipa包"
-#.app生成后的路径app_name_path=$build_path/build/Release-iphoneos/${project_name}.app#.ipa生成后的路径ipa_name_path=$build_path/build/Release-iphoneos/${project_name}.ipa
-#生成ipa包xcrun -sdk iphoneos PackageApplication -v $app_name_path -o $ipa_name_path
+#.app生成后的路径
+app_name_path=$build_path/build/Release-iphoneos/${project_name}.app
+#.ipa生成后的路径
+
+root_path=`pwd`
+
+ipa_name_path=$root_path/build/Release-iphoneos/${project_name}.ipa
+#生成ipa包
+xcrun -sdk iphoneos PackageApplication -v $app_name_path -o $ipa_name_path
 echo "制作ipa包完成......................."
